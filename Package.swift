40808f1c40c4c42e5be324f67ee9ddaa1ae24279
s1ddok <siddok@gmail.com>
2017-06-25 22:51:27 +0300

    Migrate to Manifest v4 API

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "Cpng"
+    name: "Cpng",
+    products: [
+        .library(
+            name: "Cpng",
+            type: .static,
+            targets: ["Cpng"]),
+        ],
+    targets: [
+        .target(
+            name: "Cpng",
+            path: ".",
+            sources: ["Cpng"],
+            publicHeadersPath: "Cpng/include"
+        )
+    ]
 )
+
